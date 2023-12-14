9fbea3013b797c6a50b5202605164fe32efc186c
Alexander Pridava <sanyapridava@mail.ru>
2023-12-14 04:14:59 +0300

    AbstractUser create for user with different role

---
+++ kubsu/kubsu/AbstractUser.py
@@ -0,0 +1,88 @@
+'''
+AbstractUser this is class what definition all user in system
+'''
+from Account.models import *
+from Account.serializers import *
+import re
+
+class Error:
+    data = {
+        'message': 'User not found!'
+    }
+
+class AbstractUser:
+
+    def __init__(self, login, password):
+        self.login = login
+        self.password = password
+  
+class JSONstudent(AbstractUser):
+  
+    def check_login(self): #for student this is id
+        check_login = re.match(r's\d{7}', self.login)
+
+        if check_login == None or self.login != check_login.group():
+            return False
+        return True
+    
+    def get(self):
+        if self.check_login():
+            datauser = Students.objects.filter(id_student=self.login, password=self.password).first()
+            try:
+                if datauser.id_student != '':
+                    return StudentSerializer(datauser)
+            except AttributeError:
+                return Error
+        return False
+    
+class JSONteacher(AbstractUser):
+    
+    def check_login(self): #email
+
+        pattern = r'^[\w\.-]+@[\w\.-]+\.\w+$'
+        
+        if re.match(pattern, self.login):
+            return True
+        return False
+    
+    def get(self):
+        if self.check_login():
+            datauser = Teachers.objects.filter(email=self.login, password=self.password).first()
+            try:
+                if datauser.email != '':
+                    return TeacherSerializer(datauser)
+            except AttributeError:
+                return Error
+        return False
+
+class JSONdecan(AbstractUser):
+    
+    def check_login(self):
+        #For decans special login in database
+        if self.login == "superuser":
+            return True
+        return False
+    
+    def get(self):
+        if self.check_login():
+            datauser = Decan.objects.filter(login=self.login, password=self.password).first()
+            try:
+                if datauser.login != '':
+                    return DecanSerializer(datauser)
+            except AttributeError:
+                return Error
+        return False
+####
+class UserSystem(AbstractUser):
+
+    classlist = [JSONdecan, JSONstudent, JSONteacher]
+
+    def get(self):
+        for User in UserSystem.classlist:
+            datauser = User(self.login, self.password).get()
+            if datauser != False:
+                return datauser
+            
+
+
+    
--- kubsu/kubsu/views.py
+++ kubsu/kubsu/views.py
@@ -6,39 +6,15 @@ from .serializers import *
 from Account.serializers import *
 import jwt
 import re #Check login 
+from .AbstractUser import *
 
 @api_view(['POST'])  #Auth user in system
 def AuthUser(request):
 
-    #Who is have login? Student, Teacher or Decan
-    def check_login_student(login): #for student this is id
-        check_login = re.match(r's\d{7}', login)
-
-        if check_login == None or login != check_login.group():
-            return False
-        return True
-    
-    def check_login_teacher(email): #email
-
-        pattern = r'^[\w\.-]+@[\w\.-]+\.\w+$'
-        
-        if re.match(pattern, email):
-            return True
-        return False
-    
-    def check_login_decan(login):
-        #For decans special login in database
-        return True
-
     login = request.data['login']
     password = request.data['password']
 
-    if check_login_student(login):
-        datauser = StudentSerializer(Students.objects.filter(id_students=login, password=password).first())
-    elif check_login_teacher(login):
-        datauser = TeacherSerializer(Teachers.objects.filter(email=login, password=password).first())
-    elif check_login_decan(login):
-        datauser = DecanSerializer(Decan.objects.filter(id_faculty=login, password=password).first())
+    datauser = UserSystem(login, password).get()
 
-    return Response(datauser)
+    return Response(datauser.data)
 
